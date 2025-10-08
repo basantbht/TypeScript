bdaa39c9ac06049c4c71bb1dc6c84cfdf41b1e85
basantbht <bhattbasant012@gmail.com>
2025-10-08 22:26:04 +0545

    functions

---
+++ L4_functions/app.ts
@@ -0,0 +1,40 @@
+//! Function return type
+
+function add(num1: number,num2: number): number {
+    return num1 + num2; // return 30
+}   
+
+// console.log(add(10,20))
+
+function greet(name: string): void{
+    console.log(`Hi, ${name}`);
+}
+
+// greet("basant");
+
+// let combineFunction : Function;
+// combineFunction = add;
+// combineFunction = function(){};
+// combineFunction = greet;
+// console.log(combineFunction(1,20))
+
+
+// Good Practice
+
+let combineFunction : (a: number,b: number) => number;
+combineFunction = add;
+
+console.log(combineFunction(100,200));
+
+// Function type and callbacks
+
+type CB = (n:number) => void;
+
+function addHandle(num1:number, num2:number, cb: CB){
+    const result = num1 + num2;
+    cb(result);
+}
+
+addHandle(10,20, (result: number) => {
+    console.log(result);
+});
